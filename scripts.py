9ad8dee62283c7154365f4aea4735ed8c0eb6d9c
SVISHNUVARDHAN3610 <105763583+SVISHNUVARDHAN3610@users.noreply.github.com>
2026-08-11 14:55:17 +0530

    testing

---
+++ scripts.py
@@ -0,0 +1,20 @@
+import json
+
+
+input_file, output_file= "training_logs.jsonl" , "training_logs_every_100.jsonl"
+
+with open(input_file, "r", encoding="utf-8") as infile, \
+     open(output_file, "w", encoding="utf-8") as outfile:
+
+    for line in infile:
+        line = line.strip()
+
+        if not line:
+            continue
+
+        record = json.loads(line)
+
+        if record["step"] % 100 == 0:
+            outfile.write(json.dumps(record) + "\n")
+
+print(f"Filtered logs saved to: {output_file}")
